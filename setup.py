041c5b6f58c984d66ed09d58bba9fcc2138b14fa
Sune Debel <sad@archii.it>
2018-01-18 15:49:03 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name='serum',
   packages=['serum'],
-  version='0.1',
+  version='0.1.1',
   description='A random test lib',
   author='Sune Debel',
   license='MIT',
