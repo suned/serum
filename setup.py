4dd7cc5366a06782d1a9152a6e5f2b60b8c3d9cf
Sune Debel <sad@archii.it>
2018-01-19 22:09:11 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 setup(
   name='serum',
   packages=['serum'],
-  version='0.1.1',
-  description='A random test lib',
+  version='0.2.0',
+  description='Dependency Injection library for Python 3',
   author='Sune Debel',
   license='MIT',
   author_email='sd@dybro-debel.dk',
   python_requires='>=3.5',
   url='https://github.com/suned/serum',
-  download_url='https://github.com/suned/serum/archive/0.1.tar.gz',
+  download_url='https://github.com/suned/serum/archive/0.2.0.tar.gz',
   keywords=['dependency-injection', 'solid', 'inversion-of-control'],
   classifiers=[],
 )
